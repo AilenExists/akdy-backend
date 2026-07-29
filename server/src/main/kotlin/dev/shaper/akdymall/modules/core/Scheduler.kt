a6dc183e891045b18a639a9553355d08ff129c89
shaper12340w <naver.36.20@gmail.com>
2026-07-29 13:22:09 +0900

    fix: db-scheduler 오류 수정

--- server/src/main/kotlin/dev/shaper/akdymall/modules/core/Scheduler.kt
+++ server/src/main/kotlin/dev/shaper/akdymall/modules/core/Scheduler.kt
@@ -7,7 +7,31 @@ import org.koin.ktor.ext.inject
 import javax.sql.DataSource
 
 fun Application.configureScheduler() {
+    fun ensureSchedulerTable(dataSource: DataSource) {
+        dataSource.connection.use { conn ->
+            conn.createStatement().use { stmt ->
+                stmt.execute("""
+                CREATE TABLE IF NOT EXISTS scheduled_tasks (
+                  task_name text not null,
+                  task_instance text not null,
+                  task_data bytea,
+                  execution_time timestamp with time zone not null,
+                  picked BOOLEAN not null,
+                  picked_by text,
+                  last_success timestamp with time zone,
+                  last_failure timestamp with time zone,
+                  consecutive_failures INT,
+                  last_heartbeat timestamp with time zone,
+                  version BIGINT not null,
+                  priority SMALLINT,
+                  PRIMARY KEY (task_name, task_instance)
+                )
+            """.trimIndent())
+            }
+        }
+    }
     val dataSource: DataSource by inject()
+    ensureSchedulerTable(dataSource)
     val scheduler = Scheduler
         .create(
             dataSource,
